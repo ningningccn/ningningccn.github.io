c4eccd31cce0c50d941bc224e89801911dd627f0
ningningccn <44571910+ningningccn@users.noreply.github.com>
2020-11-26 13:12:27 +0800

    detel

--- HTML/Flex_project/RWD_FOOD/js/all.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.menu-icon').click(function() {
-    $(this).toggleClass ('animated');
-    $('.inside-menu').slideToggle(500);
-  });
-  // $('.fa-heart').click(function() {
-  //   $(this).toggleClass("colored");
-  // });
-})
